36183bb69c039ac6234c0f823146401d4d1f00ff
nickjevershed <nick.evershed@gmail.com>
2019-09-23 11:32:10 +1000

    testing

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
-// js code here
+import Ractive from 'ractive'
+import template from '../templates/template.html'
+
+var database = {"things":"stuff"}
+
+var ractive = new Ractive({
+            events: { 
+            el: '#app',
+            data: database,
+            template: template
+        }})
+
+console.log("blah")
